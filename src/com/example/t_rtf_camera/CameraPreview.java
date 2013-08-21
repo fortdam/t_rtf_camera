972e81b293cb2a20410d91c26b06ec562ce988d9
zhiming.tang <zhiming.tang@tcl.com>
2013-08-21 14:32:36 +0800

    Using CPU to generate the grey-scale, sepia-tone and revert effect in main thread, very very slow

--- src/com/example/t_rtf_camera/CameraPreview.java
+++ src/com/example/t_rtf_camera/CameraPreview.java
@@ -1,8 +1,11 @@
 package com.example.t_rtf_camera;
 
 import java.io.IOException;
+import java.util.List;
+
 
 import android.content.Context;
+import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.util.Log;
 import android.view.SurfaceHolder;
@@ -14,8 +17,10 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 	
     private SurfaceHolder mHolder;
     private Camera mCamera;
+    
+    private Camera.PreviewCallback mPreviewCallback = null;
 
-    public CameraPreview(Context context, Camera camera) {
+    public CameraPreview(Context context, Camera camera, Camera.PreviewCallback callback) {
         super(context);
         mCamera = camera;
 
@@ -23,13 +28,47 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         // underlying surface is created and destroyed.
         mHolder = getHolder();
         mHolder.addCallback(this);
+        mPreviewCallback = callback;
     }
+    
+    /*private Camera.PreviewCallback mPreviewCallback = new Camera.PreviewCallback(){
+        @Override
+        public void onPreviewFrame(byte[] data, Camera camera) {
+            //Log.d(_.TAG, "onPreviewCallback()");
+            
+			Log.e("Tang zhiming", "echo here");
+			//mCamera.setPreviewCallback(this);
+
+        }
+    };*/
 
     public void surfaceCreated(SurfaceHolder holder) {
         // The Surface has been created, now tell the camera where to draw the preview.
         try {
+        	Camera.Parameters params = mCamera.getParameters();
+
+            List<Camera.Size> sizes = params.getSupportedPreviewSizes();
+            for (int i=0; i < sizes.size(); i++){
+                Camera.Size size = sizes.get(i);
+            }
+            
+        	
+        	params.setPreviewFormat(ImageFormat.RGB_565);
+        	int width = params.getPreviewSize().width;
+        	int height = params.getPreviewSize().height;
+        	
+        	mCamera.setParameters(params);
+        	
+        	byte[] buffer = new byte[width*height*2];
+        	
+        	mCamera.addCallbackBuffer(buffer);
+            
             mCamera.setPreviewDisplay(holder);
-            mCamera.startPreview();
+            mCamera.startPreview();         
+            
+            mCamera.setPreviewCallback(mPreviewCallback);
+
+
         } catch (IOException e) {
             Log.d(TAG, "Error setting camera preview: " + e.getMessage());
         }
@@ -42,7 +81,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
         // If your preview can change or rotate, take care of those events here.
         // Make sure to stop the preview before resizing or reformatting it.
-
+    	
         if (mHolder.getSurface() == null){
           // preview surface does not exist
           return;
@@ -62,6 +101,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         try {
             mCamera.setPreviewDisplay(mHolder);
             mCamera.startPreview();
+        	mCamera.setPreviewCallback(mPreviewCallback);
 
         } catch (Exception e){
             Log.d(TAG, "Error starting camera preview: " + e.getMessage());
--- src/com/example/t_rtf_camera/PreviewActivity.java
+++ src/com/example/t_rtf_camera/PreviewActivity.java
@@ -4,15 +4,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.hardware.Camera;
+import android.util.Log;
 import android.view.Menu;
 import android.view.SurfaceView;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.os.SystemClock;
 
+public class PreviewActivity extends Activity implements Camera.PreviewCallback{
 
-
-public class PreviewActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,10 +26,14 @@ public class PreviewActivity extends Activity {
         mCamera.setDisplayOrientation(90);
 
         // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        
+        mPreview = new CameraPreview(this, mCamera, this);
+                
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
+        
+        mBlackWhiteView = (ImageView) findViewById(R.id.bnw_preview);
+        mSepiaToneView = (ImageView) findViewById(R.id.sepia_tone_preview);
+        mRevertView = (ImageView) findViewById(R.id.revert_preview);
 	}
 
 	@Override
@@ -61,6 +68,77 @@ public class PreviewActivity extends Activity {
 	    return c; // returns null if camera is unavailable
 	}
 	
+	private int min(int a, int b){
+		if (a>b){
+			return b;
+		}
+		else{
+			return a;
+		}
+	}
+
+	@Override
+	public void onPreviewFrame(byte[] data, Camera camera) {
+		// TODO Auto-generated method stub
+		long startTime = SystemClock.uptimeMillis();
+		long midTime = 0;
+		long endTime = 0;
+		
+		Camera.Parameters params = camera.getParameters();
+		int width = params.getPreviewSize().width;
+		int height = params.getPreviewSize().height;
+		
+		int[] colorsBNW = new int[width*height];
+		int[] colorsSepiaTone = new int[width*height];
+		int[] colorsRevert = new int[width*height];
+		
+		for (int i=0; i<width*height; i++){
+			
+			int red = (data[i*2+1] & 0xf8);
+			int green = (data[i*2+1] & 0x07)<<5 | (data[i*2] & 0xe0)>>3;
+			int blue = (data[i*2] & 0x1f) << 3;
+			int greyScale = (red+green+blue)/3;
+			int pos = (i/width) + (i%width)*height;  //rotate 90 degree
+
+			colorsBNW[pos] = 0xff000000;
+			colorsBNW[pos] |= greyScale << 16;
+			colorsBNW[pos] |= greyScale << 8;
+			colorsBNW[pos] |= greyScale;
+			
+			colorsSepiaTone[pos] = 0xff000000;
+			colorsSepiaTone[pos] |= (min((int)(red*0.393f + green*0.769f + blue*0.189f), 255)) << 16;
+			colorsSepiaTone[pos] |= (min((int)(red*0.349f + green*0.686f + blue*0.168f), 255)) << 8;
+			colorsSepiaTone[pos] |= (min((int)(red*0.272f + green*0.534 + blue*0.131f), 255));
+			
+			colorsRevert[pos] = 0xff000000;
+			colorsRevert[pos] |= (0xff ^ red)<<16;
+			colorsRevert[pos] |= (0xff ^ green)<<8;
+			colorsRevert[pos] |= 0xff ^ blue;
+		}
+		
+		midTime = SystemClock.uptimeMillis();
+		
+		Bitmap bmpBNW = Bitmap.createBitmap(colorsBNW, height, width, Bitmap.Config.ARGB_8888);
+		mBlackWhiteView.setImageBitmap(bmpBNW);
+		
+		Bitmap bmpSepiaTone = Bitmap.createBitmap(colorsSepiaTone, height, width, Bitmap.Config.ARGB_8888);
+		mSepiaToneView.setImageBitmap(bmpSepiaTone);
+		
+		Bitmap bmpRevert = Bitmap.createBitmap(colorsRevert, height, width, Bitmap.Config.ARGB_8888);
+		mRevertView.setImageBitmap(bmpRevert);
+		
+		endTime = SystemClock.uptimeMillis();
+		
+		Log.e("Tang zhiming", "data process="+(midTime-startTime)+" , total="+(endTime-startTime));
+		
+		
+	}
+	
+	
+	
 	private Camera mCamera;
 	private CameraPreview mPreview;
+	private ImageView mBlackWhiteView;
+	private ImageView mSepiaToneView;
+	private ImageView mRevertView;
 }
