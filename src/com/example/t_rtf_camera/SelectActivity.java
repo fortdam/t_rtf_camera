dbe2f7dfaa93519a336ab75749c4809d31baafa4
dltang <zhiming.tang@tcl.com>
2013-09-29 17:22:56 +0800

    Add an entry point for multiple technology implementations

---
+++ src/com/example/t_rtf_camera/SelectActivity.java
@@ -0,0 +1,37 @@
+package com.example.t_rtf_camera;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class SelectActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_select);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_select, menu);
+		return true;
+	}
+
+	public void startRenderScript(View view){
+		Intent intent = new Intent(this, PreviewActivity.class);
+		startActivity(intent);
+	}
+	
+	public void startOpenGL_ES(View view){
+		Intent intent = new Intent(this, GLPreviewActivity.class);
+		startActivity(intent);
+	}
+	
+	public void startNDK(View view){
+		
+	}
+}
